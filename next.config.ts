958655137e44877e60f224b81909a2be0dfdf69b
Magnus Junghard Huttu <magnushuttu@gmail.com>
2025-03-04 22:14:00 +0100

    move path for svg-path-length-calculator

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  redirects: async () => {
+    return [
+      {
+        source: '/svg-length',
+        destination: '/svg-path-length-calculator',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;

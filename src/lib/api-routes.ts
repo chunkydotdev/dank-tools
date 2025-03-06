f0d09d8e9e2bb5a5760db887a8752e237d7f0719
Magnus Junghard Huttu <magnushuttu@gmail.com>
2025-03-06 20:45:52 +0100

    update .env

--- src/lib/api-routes.ts
+++ src/lib/api-routes.ts
@@ -1,4 +1,5 @@
-export const apiRoot = process.env.API_PATH || "http://localhost:3001/api";
+export const apiRoot =
+  process.env.NEXT_PUBLIC_API_PATH || "http://localhost:3001/api";
 
 export const apiRoutes = {
   polls: `${apiRoot}/polls`,
